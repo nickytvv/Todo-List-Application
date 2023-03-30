45a00570c552184fe73131aaa864bc4429b7927c
Nicholas Sanchez <nickasanchez011@gmail.com>
2023-03-30 17:03:43 +0000

    Created ToDO Assignment

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Task = ({ index, task, deleteTask }) => {
+  const [hovered, setHovered] = useState(false);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <li onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+      <div>{task}</div>
+      {hovered && <button onClick={() => deleteTask(index)}>Delete</button>}
+    </li>
+  );
 };
 
-export default Home;
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (e) => {
+    e.preventDefault();
+    const newTask = e.target.elements.task.value.trim();
+    if (newTask) {
+      setTasks([...tasks, newTask]);
+      e.target.elements.task.value = '';
+    }
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={addTask}>
+        <input type="text" name="task" placeholder="Add a task" />
+        <button type="submit">Add Task</button>
+      </form>
+      {tasks.length === 0 ? (
+        <p>No tasks, add a task</p>
+      ) : (
+        <ul>
+          {tasks.map((task, index) => (
+            <Task key={index} index={index} task={task} deleteTask={deleteTask} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
